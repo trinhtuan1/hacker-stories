82903f8bb51d3974cfc1a36b8d870c10d0c2da01
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-01-31 10:58:55 +0700

    done with memoized handler

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if(!searchTerm) return;
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
     fetch(`${API_ENDPOINT}${searchTerm}`)
@@ -61,6 +61,10 @@ const App = () => {
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
   }, [searchTerm]);
 
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
+  
   const handleRemoveStory = item => {
     dispatchStories({
       type: 'REMOVE_STORY',
