d97e4a87e3a297cf51fc5688bf06dad14d7f4132
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-01-31 11:24:16 +0700

    done explicit data fetching

--- src/App.js
+++ src/App.js
@@ -38,19 +38,25 @@ const storiesReducer = (state, action) => {
 
 const App = () => {
   const [ searchTerm, setSearchTerm ] = React.useState('');
+  const [ url, setUrl ] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
   const [ stories, dispatchStories ] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
-  const handleSearch = event => {
+  const handleSearchInput = event => {
     setSearchTerm(event.target.value);
   };
 
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`)
+  };
+
   const handleFetchStories = React.useCallback(() => {
-    if(!searchTerm) return;
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -59,7 +65,7 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -71,10 +77,6 @@ const App = () => {
       payload: item
     });
   };
-
-  const searchedStories = stories.data.filter(story => (
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  ));
   
   return (
     <div>
@@ -82,12 +84,19 @@ const App = () => {
       <InputWithLabel
         id="search"
         value={ searchTerm }
-        onInputChange={ handleSearch }
         isFocused
+        onInputChange={ handleSearchInput }
       >
         <p>HelloEveryone</p>
         Search:&nbsp;
       </InputWithLabel>
+      <button
+        type="button"
+        disabled={ !searchTerm }
+        onClick={ handleSearchSubmit }
+      >
+        Submit
+      </button>
       {
         stories.isError && <p>Something wen wrong...</p>
       }
